787963962e14f91cae2527bf5d22b1d13f206e63
Victor Ojeda <ojedavd@gmail.com>
2019-05-14 16:28:18 -0300

    extending the record rules

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/todo_access_rules.xml',
         'templates.xml',
         'todo_view.xml',
     ],
